c82dea10dbc5ec0c5dcd17d660713e7e0cef2c45
Kristina <krislady1412@gmail.com>
2022-11-13 10:12:43 +0600

    Дз 4

---
+++ src/main/java/stackL4/Task1.java
@@ -0,0 +1,20 @@
+package stackL4;
+
+import java.util.Stack;
+
+public class Task1 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char i : s.toCharArray()) {
+            if (i == '(')
+                stack.push(')');
+            else if (i == '{')
+                stack.push('}');
+            else if (i == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != i)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
---
+++ src/main/java/stackL4/Task2.java
@@ -0,0 +1,34 @@
+package stackL4;
+
+import java.util.Stack;
+
+public class Task2 {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for (int i = 0; i < tokens.length; i++) {
+            switch (tokens[i]) {
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+
+                case "-":
+                    stack.push(-stack.pop() + stack.pop());
+                    break;
+
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+
+                case "/":
+                    int n1 = stack.pop(), n2 = stack.pop();
+                    stack.push(n2 / n1);
+                    break;
+
+                default:
+                    stack.push(Integer.parseInt(tokens[i]));
+            }
+        }
+        return stack.pop();
+    }
+}
